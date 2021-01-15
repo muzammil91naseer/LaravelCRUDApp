09e2f20e6c9601af5f85536339764566874fe341
Muzammil Naseer <muzammilnaseer91@gmail.com>
2021-01-15 19:03:31 +0500

    seeding complete

--- database/seeders/EmployeesTableSeeder.php
+++ database/seeders/EmployeesTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class EmployeesTableSeeder extends Seeder
 {
@@ -13,6 +15,20 @@ class EmployeesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i=1; $i < 11; $i++)
+        {
+            for($j=1; $j< 21; $j++)
+            {
+                DB::table('employees')->insert([
+                    "Name"=>Str::random(10),
+                    "Designation"=>Str::random(15),
+                    "Address"=>Str::random(20),
+                    "company_id"=>$i,
+
+                ]);
+
+            }
+
+        }
     }
 }
