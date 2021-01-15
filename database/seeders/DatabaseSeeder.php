2d34a0535fde7362d0a7dd60abcbd37979e3d8f1
Muzammil Naseer <muzammilnaseer91@gmail.com>
2021-01-15 23:52:47 +0500

    modification in  DatbaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Company;
+use App\Models\User;
+use App\Models\Employee;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //truncate all past data
+        User::query()->truncate();
+        Company::query()->truncate();
+        Employee::query()->truncate();
+
+        //insert admin data
+        $admin = new User;
+        $admin->name="Admin";
+        $admin->email="admin@doorhub.io";
+        $admin->password="Doorhub123456";
+        $admin->is_admin=true;
+        $admin->save();
+
+        //insert company then its employees data
+        for($i=0; $i<10; $i++)
+        {
+            $company = new Company;
+            $company->Name= Str::random(10);
+            $company->Address= Str::random(20);
+            $company->IndustryTitle= Str::random(15);
+            $company->save();
+            $last_id = $company->id;
+
+            for($j=0; $j<20; $j++)
+            {
+                $employee = new Employee;
+                $employee->Name = Str::random(10);
+                $employee->Address = Str::random(10);
+                $employee->Designation = Str::random(10);
+                $employee->company_id = $last_id;
+                $employee->save();
+            }
+
+        }
+
     }
 }
